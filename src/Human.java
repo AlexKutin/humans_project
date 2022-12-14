a383ccaa0fddbb49ab4dc74705f42b80bbaa6fa4
Alexey Kutin <kutinalexeydm@gmail.com>
2022-12-14 22:08:27 +0300

    Homework 1.2

--- src/Human.java
+++ src/Human.java
@@ -3,15 +3,18 @@ public class Human {
     private String name;
     private String town;
 
-    public Human(int yearOfBirth, String name, String town) {
+    private String job;
+
+    public Human(int yearOfBirth, String name, String town, String job) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.job = job;
     }
 
     @Override
     public String toString() {
-        return String.format("Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Будем знакомы!",
-                name, town, yearOfBirth);
+        return String.format("Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Я работаю на должности %s. Будем знакомы!",
+                name, town, yearOfBirth, job);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Human maxim = new Human(1987, "Максим", "Минск");
-        Human ann = new Human(1993, "Аня", "Москва");
-        Human katya = new Human(1994, "Катя", "Калининград");
-        Human artem = new Human(1995, "Артем", "Москва");
+        Human maxim = new Human(1987, "Максим", "Минск", "бренд-менеджер");
+        Human ann = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+        Human katya = new Human(1994, "Катя", "Калининград", "продакт-менеджер");
+        Human artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
 
         System.out.println(maxim);
         System.out.println(ann);
