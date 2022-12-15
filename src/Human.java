f29681df45c51b1e1d799e7b0624d9e039e35f2f
Alexey Kutin <kutinalexeydm@gmail.com>
2022-12-15 20:40:03 +0300

    Homework 3.1 - added checks to the constructors

--- src/Human.java
+++ src/Human.java
@@ -6,15 +6,27 @@ public class Human {
     private String job;
 
     public Human(int yearOfBirth, String name, String town, String job) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.job = job;
+        if (yearOfBirth > 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+        this.name = checkIsNotEmptyAndFill(name);
+        this.town = checkIsNotEmptyAndFill(town);
+        this.job = checkIsNotEmptyAndFill(job);
     }
 
     @Override
     public String toString() {
         return String.format("Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Я работаю на должности %s. Будем знакомы!",
                 name, town, yearOfBirth, job);
+    }
+
+    private String checkIsNotEmptyAndFill(String str) {
+        if (str == null || str.isBlank()) {
+            return "Информация не указана";
+        } else {
+            return str;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Human ann = new Human(1993, "Аня", "Москва", "методист образовательных программ");
         Human katya = new Human(1994, "Катя", "Калининград", "продакт-менеджер");
         Human artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
+        Human andrey = new Human(-8, "Андрей", "", null);
 
         System.out.println(maxim);
         System.out.println(ann);
         System.out.println(katya);
         System.out.println(artem);
+        System.out.println(andrey);
     }
 }
